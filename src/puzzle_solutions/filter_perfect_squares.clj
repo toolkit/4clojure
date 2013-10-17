ef60cb9611ccd6f41837879dfbdc2cca32590573
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 19:53:17 +0100

    Filter Perfect Squares

---
+++ src/puzzle_solutions/filter_perfect_squares.clj
@@ -0,0 +1,15 @@
+(ns puzzle-solutions.filter-perfect-squares
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 74 - Filter Perfect Squares
+;; http://www.4clojure.com/problem/74
+
+(def __ (fn [s]
+          (apply str
+                 (interpose ","
+                            (filter #(let [l (int (Math/sqrt %))]
+                                       (= % (* l l)))
+                                    (map #(read-string %) (.split s ",")))))))
+
+(is (= (__ "4,5,6,7,8,9") "4,9"))
+(is (= (__ "15,16,25,36,37") "16,25,36"))
