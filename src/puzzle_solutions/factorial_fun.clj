9a8adb9b17ca75fb74e60ffdcea8b294130445f3
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 19:47:25 +0100

    Factorial Fun

---
+++ src/puzzle_solutions/factorial_fun.clj
@@ -0,0 +1,12 @@
+(ns puzzle-solutions.factorial-fun
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 42 - Factorial Fun
+;; http://www.4clojure.com/problem/42
+
+(def __ #(apply * (range 1 (inc %))))
+
+(is (= (__ 1) 1))
+(is (= (__ 3) 6))
+(is (= (__ 5) 120))
+(is (= (__ 8) 40320))
