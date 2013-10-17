28ec3d3c04448b681425928c1ceeb73fcfcffe7c
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 14:37:38 +0100

    A nil key

---
+++ src/puzzle_solutions/a_nil_key.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.a-nil-key
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 134 - A nil key
+;; http://www.4clojure.com/problem/134
+
+(def __ #(nil? (%1 %2 :not-found)))
+
+(is (true?  (__ :a {:a nil :b 2})))
+(is (false? (__ :b {:a nil :b 2})))
+(is (false? (__ :c {:a nil :b 2})))
