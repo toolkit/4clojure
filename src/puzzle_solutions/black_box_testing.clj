e1d32fb87074850e485ba766f43f9d14934a6bea
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 15:47:22 +0100

    Tidy up

--- src/puzzle_solutions/black_box_testing.clj
+++ src/puzzle_solutions/black_box_testing.clj
@@ -1,27 +1,17 @@
-(ns puzzle-solutions.black-box-testing)
+(ns puzzle-solutions.black-box-testing
+  (:require [clojure.test :refer [is]]))
 
 ;; Problem 65 - Black box testing
 ;; http://www.4clojure.com/problem/65
 
-(map #(cond
-       (reversible? %) :vector
-       (associative? %) :map
-       (= % (apply list %)) :list
-       :else :set) [[] #{} {} ()])
+(def __ #(cond
+  (reversible? %) :vector
+  (associative? %) :map
+  (= % (apply list %)) :list
+  :else :set))
 
-;; Problem 58 - function composition
-(((fn [& fs]
-    (reduce (fn [f g]
-              #(f (apply g %&))) fs))
-   #(.toUpperCase %) #(apply str %) take) 5 "hello world")
-
-
-;; Problem 150 - Palindromic numbers
-(defn to-num [v]
-  (reduce + (map * (reverse v) (iterate (partial * 10) 1))))
-
-(defn to-seq [n]
-  (map #(- (int %) 48) (str n)))
-
-(to-seq 123)
-(to-num (to-seq 123))
+(is (= :map (__ {:a 1, :b 2})))
+(is (= :list (__ (range (rand-int 20)))))
+(is (= :vector (__ [1 2 3 4 5 6])))
+(is (= :set (__ #{10 (rand-int 5)})))
+(is (= [:map :set :vector :list] (map __ [{} #{} [] ()])))
