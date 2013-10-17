8bbf25459a601392781930a1e4b101ac7454cf61
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 15:22:22 +0100

    Balancing brackets

--- src/puzzle_solutions/balancing_brackets.clj
+++ src/puzzle_solutions/balancing_brackets.clj
@@ -1,16 +1,26 @@
-(ns puzzle-solutions.balancing-brackets)
+(ns puzzle-solutions.balancing-brackets
+  (:require [clojure.test :refer [is]]))
 
 ;; Problem 177 - Balancing Brackets
 ;; http://www.4clojure.com/problem/177
 
-(#(let [a (clojure.string/replace % #"[^\(^\)^\[^\]^\{^\}]" "")
-        b (clojure.string/replace a #"(\(\)|\[\]|\{\})" "")]
-    (if (empty? b)
-      true
-      (if (= b %)
-        false
-        (recur b)))) "class Test {
+(def __ #(let [a (clojure.string/replace % #"[^\(^\)^\[^\]^\{^\}]" "")
+       b (clojure.string/replace a #"(\(\)|\[\]|\{\})" "")]
+   (if (empty? b)
+     true
+     (if (= b %)
+       false
+       (recur b)))))
+
+(is (__ "This string has no brackets."))
+(is (__ "class Test {
       public static void main(String[] args) {
         System.out.println(\"Hello world.\");
       }
-    }")
+    }"))
+(is (not (__ "(start, end]")))
+(is (not (__ "())")))
+(is (not (__ "[ { ] } ")))
+(is (__ "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))"))
+(is (not (__ "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))")))
+(is (not (__ "[")))
