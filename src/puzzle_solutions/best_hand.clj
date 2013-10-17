e10ac83651ba290b7f25a827aaacafcc6c61e4f3
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 15:33:14 +0100

    Best hand

--- src/puzzle_solutions/best_hand.clj
+++ src/puzzle_solutions/best_hand.clj
@@ -1,9 +1,10 @@
-(ns puzzle-solutions.best-hand)
+(ns puzzle-solutions.best-hand
+  (:require [clojure.test :refer [is]]))
 
 ;; Problem 178 - Best Hand **
 ;; http://www.4clojure.com/problem/178
 
-(defn best-hand [cards]
+(def __ (fn [cards]
   (let [flush? (apply = (map first cards))
         ranks (map (zipmap "23456789TJQKA" (range 13)) (map last cards))
         freqs (frequencies (vals (frequencies ranks)))
@@ -18,16 +19,15 @@
       (= {3 1 1 2} freqs)     :three-of-a-kind
       (= {2 2 1 1} freqs)     :two-pair
       (= {2 1 1 3} freqs)     :pair
-      :else                   :high-card)))
+      :else                   :high-card))))
 
-(map best-hand
-  [["HA" "D2" "H3" "C9" "DJ"]
-   ["HA" "DA" "HQ" "SJ" "HT"]
-   ["HA" "DA" "HQ" "SQ" "HT"]
-   ["HA" "DA" "CA" "HJ" "HT"]
-   ["HA" "DK" "HQ" "HJ" "HT"]
-   ["HA" "HK" "H2" "H4" "HT"]
-   ["HA" "DA" "CA" "HJ" "DJ"]
-   ["HA" "DA" "CA" "SA" "DJ"]
-   ["HA" "H2" "H3" "H4" "H5"]
-   ["HA" "HK" "HQ" "HJ" "HT"]])
+(is (= :high-card (__ ["HA" "D2" "H3" "C9" "DJ"])))
+(is (= :pair (__ ["HA" "HQ" "SJ" "DA" "HT"])))
+(is (= :two-pair (__ ["HA" "DA" "HQ" "SQ" "HT"])))
+(is (= :three-of-a-kind (__ ["HA" "DA" "CA" "HJ" "HT"])))
+(is (= :straight (__ ["HA" "DK" "HQ" "HJ" "HT"])))
+(is (= :straight (__ ["HA" "H2" "S3" "D4" "C5"])))
+(is (= :flush (__ ["HA" "HK" "H2" "H4" "HT"])))
+(is (= :full-house (__ ["HA" "DA" "CA" "HJ" "DJ"])))
+(is (= :full-house (__ ["HA" "DA" "CA" "HJ" "DJ"])))
+(is (= :full-house (__ ["HA" "DA" "CA" "HJ" "DJ"])))
