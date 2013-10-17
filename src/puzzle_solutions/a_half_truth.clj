df7517f368190984032c1e454f3a570cbd5a3471
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 14:31:20 +0100

    A Half-Truth

---
+++ src/puzzle_solutions/a_half_truth.clj
@@ -0,0 +1,16 @@
+(ns puzzle-solutions.a-half-truth
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 83 - A Half-Truth
+;; http://www.4clojure.com/problem/83
+
+(def __ #(and
+  (not-every? true? %&)
+  (not-every? false? %&)))
+
+(is (= false (__ false false)))
+(is (= true (__ true false)))
+(is (= false (__ true)))
+(is (= true (__ false true false)))
+(is (= false (__ true true true)))
+(is (= true (__ true true true false)))
