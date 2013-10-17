140577ccad2077482b49b791d4dd607eb48f1ff7
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 19:32:03 +0100

    Drop Every Nth Item

---
+++ src/puzzle_solutions/drop_every_nth_item.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.drop-every-nth-item
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 41 - Drop Every Nth Item
+;; http://www.4clojure.com/problem/41
+
+(def __ #(flatten (partition-all (dec %2) %2 %1)))
+
+(is (= (__ [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
+(is (= (__ [:a :b :c :d :e :f] 2) [:a :c :e]))
+(is (= (__ [1 2 3 4 5 6] 4) [1 2 3 5 6]))
