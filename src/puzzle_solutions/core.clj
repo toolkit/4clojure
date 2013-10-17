89f359f7b7329d9c93fa5c81e3e98c52b0bdce28
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 13:56:55 +0100

    tic tac toe

--- src/puzzle_solutions/core.clj
+++ src/puzzle_solutions/core.clj
@@ -1,35 +1,5 @@
 (ns puzzle-solutions.core)
 
-
-
-;; Problem 73 - Tic Tac Toe
-(defn ttt [b]
-  (letfn [(win [k]
-            (some #(every? #{k} %)
-              (concat b                                                    ; original board
-                (apply map vector b)                                       ; transposed
-                (vector (map #(nth %1 %2) b (range 3)))                    ; 1st diagonal
-                (vector (map #(nth %1 %2) b (reverse (range 3)))))))]      ; 2nd diagonal
-    (cond
-      (win :x) :x
-      (win :o) :o)))
-
-(ttt [[:e :e :e]
-      [:e :e :e]
-      [:e :e :e]])
-
-(ttt [[:x :e :o]
-      [:x :e :e]
-      [:x :e :o]])
-
-(ttt [[:e :x :e]
-      [:o :o :o]
-      [:x :e :x]])
-
-(ttt [[:x :e :e]
-      [:o :x :o]
-      [:x :e :x]])
-
 ;; Problem 138 - Squares Squared
 (defn prange [x y]
   (take-while #(<= % y) (iterate #(* % %) x)))
