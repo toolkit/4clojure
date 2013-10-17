647e888857792ce2cebf81385cf27420ac4e0001
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 18:35:26 +0100

    Dot Product

---
+++ src/puzzle_solutions/dot_product.clj
@@ -0,0 +1,12 @@
+(ns puzzle-solutions.dot-product
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 143 - Dot Product
+;; http://www.4clojure.com/problem/143
+
+(def __ #(reduce + (apply map * %&)))
+
+(is (= 0 (__ [0 1 0] [1 0 0])))
+(is (= 3 (__ [1 1 1] [1 1 1])))
+(is (= 32 (__ [1 2 3] [4 5 6])))
+(is (= 256 (__ [2 5 6] [100 10 1])))
