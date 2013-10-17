0d97133ab98f768ce337ea255a13842ab2386bf6
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 15:27:38 +0100

    Beauty is symmetry

--- src/puzzle_solutions/beauty_is_symmetry.clj
+++ src/puzzle_solutions/beauty_is_symmetry.clj
@@ -1,13 +1,23 @@
-(ns puzzle-solutions.beauty-is-symmetry)
+(ns puzzle-solutions.beauty-is-symmetry
+  (:require [clojure.test :refer [is]]))
 
 ;; Problem 96 - Beauty is symmetry
 ;; http://www.4clojure.com/problem/96
 
-(defn t [[n l r]]
+(def __ (fn [[n l r]]
   (let [m (fn m [[n l r]]
             (vector n (if (coll? r) (m r) r) (if (coll? l) (m l) l)))]
-    (= l (m r))))
+    (= l (m r)))))
 
-
-(t [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
-    [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
+(is (= (__ '(:a (:b nil nil) (:b nil nil))) true))
+(is (= (__ '(:a (:b nil nil) nil)) false))
+(is (= (__ '(:a (:b nil nil) (:c nil nil))) false))
+(is (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+          [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
+       true))
+(is (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+          [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
+       false))
+(is (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+          [2 [3 nil [4 [6 nil nil] nil]] nil]])
+   false))
