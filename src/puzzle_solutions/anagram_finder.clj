7faca94bf386009bf8706cfbc76975260c0b7a8a
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 14:46:50 +0100

    Andagram Finder

---
+++ src/puzzle_solutions/anagram_finder.clj
@@ -0,0 +1,16 @@
+(ns puzzle-solutions.anagram-finder
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 77 - Anagram Finder
+;; http://www.4clojure.com/problem/77
+
+(def __ #(set
+    (map (comp set val)
+      (remove (comp #{1} count val)
+        (group-by sort %)))))
+
+(is (= (__ ["meat" "mat" "team" "mate" "eat"])
+       #{#{"meat" "team" "mate"}}))
+
+(is (= (__ ["veer" "lake" "item" "kale" "mite" "ever"])
+   #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}}))
