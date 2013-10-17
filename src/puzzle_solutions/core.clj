aa59d00cae80fc0a7be0bc3299942258c54eb2ca
Neil Laurance <neil.laurance@gmail.com>
2013-10-17 11:09:28 +0100

    Lazy Search

--- src/puzzle_solutions/core.clj
+++ src/puzzle_solutions/core.clj
@@ -36,18 +36,6 @@
         "_ _ f _ # _ _"])
 
 
-;; Problem 108 - Lazy Search
-
-(defn tw [ & seqs ]
-  (loop [s seqs]
-    (let [f (map first s)
-          v (apply max f)]
-      (if (every? #(= v %) f)
-        v
-        (recur (map (fn [i] (drop-while #(< % v) i)) s))))))
-
-(tw (range 1 10) (range 0 10) [3 4 5])
-
 ;; Problem 131 - Sum some set subsets  - TODO
 
 (defn summations [ & sets ]
