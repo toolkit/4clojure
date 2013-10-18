07847446962dcc7d29b295504ad433981408b4aa
Neil Laurance <neil.laurance@gmail.com>
2013-10-18 21:28:09 +0100

    Flipping Out

---
+++ src/puzzle_solutions/flipping_out.clj
@@ -0,0 +1,13 @@
+(ns puzzle-solutions.flipping-out
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 46 - Flipping Out
+;; http://www.4clojure.com/problem/46
+
+(def __ #(fn [& args]
+           (apply % (reverse args))))
+
+(is (= 3 ((__ nth) 2 [1 2 3 4 5])))
+(is (= true ((__ >) 7 8)))
+(is (= 4 ((__ quot) 2 8)))
+(is (= [1 2 3] ((__ take) [1 2 3 4 5] 3)))
