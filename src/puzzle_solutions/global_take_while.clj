a74710bc1310a7ce54229dcb306b3fd83790efb3
Neil Laurance <neil.laurance@gmail.com>
2013-10-18 21:44:44 +0100

    Global Take While

--- src/puzzle_solutions/global_take_while.clj
+++ src/puzzle_solutions/global_take_while.clj
@@ -1,16 +1,21 @@
-(ns puzzle-solutions.global-take-while)
+(ns puzzle-solutions.global-take-while
+  (:require [clojure.test :refer [is]]))
 
 ;; Problem 114 - Global take while
 ;; http://www.4clojure.com/problem/114
 
-(defn gtw [n p s]
-   (take
-     (last
-       (take n (keep-indexed #(if (p %2) %) s)))
-     s))
+(def __ (fn [n p s]
+          (take
+           (last
+            (take n (keep-indexed #(if (p %2) %) s)))
+           s)))
 
-(gtw 4 #(= 2 (mod % 3))
-  [2 3 5 7 11 13 17 19 23])
-
-(gtw 3 #(some #{\i} %)
-  ["this" "is" "a" "sentence" "i" "wrote"])
+(is (= [2 3 5 7 11 13]
+   (__ 4 #(= 2 (mod % 3))
+       [2 3 5 7 11 13 17 19 23])))
+(is (= ["this" "is" "a" "sentence"]
+   (__ 3 #(some #{\i} %)
+         ["this" "is" "a" "sentence" "i" "wrote"])))
+(is (= ["this" "is"]
+   (__ 1 #{"a"}
+         ["this" "is" "a" "sentence" "i" "wrote"])))
