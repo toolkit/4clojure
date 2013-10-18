748cc48578c70d91ac4d559c7c9e70fce98b963f
Neil Laurance <neil.laurance@gmail.com>
2013-10-18 21:41:17 +0100

    Get the Caps

---
+++ src/puzzle_solutions/get_the_caps.clj
@@ -0,0 +1,12 @@
+(ns puzzle-solutions.get-the-caps
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 29 - Get the Caps
+;; http://www.4clojure.com/problem/29
+
+(def __ #(apply str (re-seq  #"\p{Upper}" %)))
+
+
+(is (= (__ "HeLlO, WoRlD!") "HLOWRD"))
+(is (empty? (__ "nothing")))
+(is (= (__ "$#A(*&987Zf") "AZ"))
