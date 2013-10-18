ce317ac7bc871fa8d726e8947f161eac5fa881b2
Neil Laurance <neil.laurance@gmail.com>
2013-10-18 22:21:24 +0100

    Infix Calculator

---
+++ src/puzzle_solutions/infix_calculator.clj
@@ -0,0 +1,13 @@
+(ns puzzle-solutions.infix-calculator
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 135 - Infix Calculator
+;; http://www.4clojure.com/problem/135
+
+(def __ (fn [x & xs]
+          (reduce #((first %2) % (second %2)) x (partition 2 xs))))
+
+(is (= 7  (__ 2 + 5)))
+(is (= 42 (__ 38 + 48 - 2 / 2)))
+(is (= 8  (__ 10 / 2 - 1 * 2)))
+(is (= 72 (__ 20 / 2 + 2 + 4 + 8 - 6 - 10 * 9)))
