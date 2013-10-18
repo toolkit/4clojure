0b5ba0c925b36d801d59a1a81b5d5cd79bf85779
Neil Laurance <neil.laurance@gmail.com>
2013-10-18 21:24:06 +0100

    Flatten a Sequence

---
+++ src/puzzle_solutions/flatten_a_sequence.clj
@@ -0,0 +1,14 @@
+(ns puzzle-solutions.flatten-a-sequence
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 28 - Flatten a Sequence
+;; http://www.4clojure.com/problem/28
+
+(def __ (fn f [c]
+          (if (coll? c)
+            (mapcat f c)
+            [c])))
+
+(is (= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))
+(is (= (__ ["a" ["b"] "c"]) '("a" "b" "c")))
+(is (= (__ '((((:a))))) '(:a)))
