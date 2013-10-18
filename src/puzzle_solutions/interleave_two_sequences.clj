be09dbf043c80163db576a61148ac3859dbb32c9
Neil Laurance <neil.laurance@gmail.com>
2013-10-18 22:28:41 +0100

    Interleave two sequences

---
+++ src/puzzle_solutions/interleave_two_sequences.clj
@@ -0,0 +1,20 @@
+(ns puzzle-solutions.interleave-two-sequences
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 39 - Interleave two sequences
+;; http://www.4clojure.com/problem/39
+
+(def __ (fn [xs ys]
+          (flatten
+           (loop [a xs
+                  b ys
+                  r []]
+             (if (or (nil? a) (nil? b))
+               r
+               (recur (next a) (next b) (conj r [(first a) (first b)])))))))
+
+
+(is (= (__ [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c)))
+(is (= (__ [1 2] [3 4 5 6]) '(1 3 2 4)))
+(is (= (__ [1 2 3 4] [5]) [1 5]))
+(is (= (__ [30 20] [25 15]) [30 25 20 15]))
