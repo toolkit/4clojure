9f5f94ee001d4269bdbec5be516559c4e93985fc
Neil Laurance <neil.laurance@gmail.com>
2013-10-19 23:53:53 +0100

    Into Camel Case

---
+++ src/puzzle_solutions/into_camel_case.clj
@@ -0,0 +1,17 @@
+(ns puzzle-solutions.into-camel-case
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 171 - Intervals
+;; http://www.4clojure.com/problem/171
+
+(def __ (fn [s]
+          (clojure.string/replace
+           s
+           #"-(.)"
+           #(clojure.string/upper-case (last %1)))))
+
+
+
+(is (= (__ "something") "something"))
+(is (= (__ "multi-word-key") "multiWordKey"))
+(is (= (__ "leaveMeAlone") "leaveMeAlone"))
