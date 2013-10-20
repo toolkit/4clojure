b89de33d44d346e721a62696b301c4bebe221e39
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 20:24:03 +0100

    Intro to Iterate

---
+++ src/puzzle_solutions/intro_to_iterate.clj
@@ -0,0 +1,9 @@
+(ns puzzle-solutions.intro-to-iterate
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 45 - Intro to Iterate
+;; http://www.4clojure.com/problem/45
+
+(def __ [1 4 7 10 13])
+
+(is (= __ (take 5 (iterate #(+ 3 %) 1))))
