4a6432af1c130aa89daec11182f34de8beedfb6f
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 20:16:23 +0100

    Intro to Destructuring 2

---
+++ src/puzzle_solutions/intro_to_destructuring2.clj
@@ -0,0 +1,10 @@
+(ns puzzle-solutions.intro-to-destructuring2
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 173 - Intro to Destructuring 2
+;; http://www.4clojure.com/problem/173
+
+(is (= 3
+       (let [[f x] [+ (range 3)]] (apply f x))
+       (let [[[f x] b] [[+ 1] 2]] (f x b))
+       (let [[f x] [inc 2]] (f x))))
