401210cbd2372362ff043f8d97e7ce40228cfa68
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 21:44:27 +0100

    Intro to Strings

---
+++ src/puzzle_solutions/intro_to_strings.clj
@@ -0,0 +1,9 @@
+(ns puzzle-solutions.intro-to-strings
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 3 - Intro to Strings
+;; http://www.4clojure.com/problem/3
+
+(def __ "HELLO WORLD")
+
+(is (= __ (.toUpperCase "hello world")))
