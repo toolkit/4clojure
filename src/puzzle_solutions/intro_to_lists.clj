6eb2a37d4f89a9259d15db65df1c679b3ae708b6
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 20:25:57 +0100

    Intro to Lists

---
+++ src/puzzle_solutions/intro_to_lists.clj
@@ -0,0 +1,7 @@
+(ns puzzle-solutions.intro-to-lists
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 4 - Intro to Lists
+;; http://www.4clojure.com/problem/4
+
+(is (= (list :a :b :c) '(:a :b :c)))
