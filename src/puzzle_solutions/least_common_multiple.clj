80622cf2f2a6e00c6405021460057255ffdf10b5
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 22:10:13 +0100

    Least Common Multiple

---
+++ src/puzzle_solutions/least_common_multiple.clj
@@ -0,0 +1,21 @@
+(ns puzzle-solutions.least-common-multiple
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 100 - Least Common Multiple
+;; http://www.4clojure.com/problem/100
+
+(def __ (fn [& xs]
+          (letfn [(gcd [a b]
+                    (let [x (max a b)
+                          y (min a b)]
+                      (if (= x y)
+                        x
+                        (gcd (- x y) y))))
+                  (lcm [a b] (/ (* a b) (gcd a b)))]
+            (reduce lcm xs))))
+
+(is (= (__ 2 3) 6))
+(is (= (__ 5 3 7) 105))
+(is (= (__ 1/3 2/5) 2))
+(is (= (__ 3/4 1/6) 3/2))
+(is (= (__ 7 5/7 2 3/5) 210))
