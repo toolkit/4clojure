28aeae298b1276606ff05f540820ffa08c5bf8a4
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 21:47:10 +0100

    Intro to Trampoline

---
+++ src/puzzle_solutions/intro_to_trampoline.clj
@@ -0,0 +1,15 @@
+(ns puzzle-solutions.intro-to-trampoline
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 76 - Intro to Trampoline
+;; http://www.4clojure.com/problem/76
+
+(def __ [1 3 5 7 9 11])
+
+(is (= __
+       (letfn
+           [(foo [x y] #(bar (conj x y) y))
+            (bar [x y] (if (> (last x) 10)
+                         x
+                         #(foo x (+ 2 y))))]
+         (trampoline foo [] 1))))
