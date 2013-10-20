10a833c8ea65b9ff4786bfcc2526895170e71045
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 21:54:34 +0100

    Last Element

---
+++ src/puzzle_solutions/last_element.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.last-element
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 19 - Last Element
+;; http://www.4clojure.com/problem/19
+
+(def __ #(nth % (dec (count %))))
+
+(is (= (__ [1 2 3 4 5]) 5))
+(is (= (__ '(5 4 3)) 3))
+(is (= (__ ["b" "c" "d"]) "d"))
