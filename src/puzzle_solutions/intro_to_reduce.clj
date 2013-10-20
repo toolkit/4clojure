c28d379c95eb66d8510c1c1e0c4917fba5785196
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 21:35:54 +0100

    Intro to Reduce

---
+++ src/puzzle_solutions/intro_to_reduce.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.intro-to-reduce
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 64 - Intro to Reduce
+;; http://www.4clojure.com/problem/64
+
+(def __ +)
+
+(is (= 15 (reduce __ [1 2 3 4 5])))
+(is (=  0 (reduce __ [])))
+(is (=  6 (reduce __ 1 [2 3])))
