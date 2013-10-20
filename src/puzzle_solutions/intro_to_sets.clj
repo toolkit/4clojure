57178c84d32bcc2b5c4b84b5a5821eb77e9f4c16
Neil Laurance <neil.laurance@gmail.com>
2013-10-20 21:40:51 +0100

    Intro to Sets

---
+++ src/puzzle_solutions/intro_to_sets.clj
@@ -0,0 +1,10 @@
+(ns puzzle-solutions.intro-to-sets
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 8 - Intro to Sets
+;; http://www.4clojure.com/problem/8
+
+(def __ #{:a :b :c :d})
+
+(is (= __ (set '(:a :a :b :c :c :c :c :d :d))))
+(is (= __ (clojure.set/union #{:a :b :c} #{:b :c :d})))
