fede8caff09b5a8c6b544f3c1d88a9994c001552
Neil Laurance <neil.laurance@gmail.com>
2013-10-22 23:33:10 +0100

    Map Construction

---
+++ src/puzzle_solutions/map_construction.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.map-construction
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 61 - Map Construction
+;; http://www.4clojure.com/problem/61
+
+(def __ #(into {} (map vector % %2)))
+
+(is (= (__ [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3}))
+(is (= (__ [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"}))
+(is (= (__ [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"}))
