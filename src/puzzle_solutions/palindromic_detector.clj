2eaa18745d1792f98dd77e4243115bcd6ef07221
Neil Laurance <neil.laurance@gmail.com>
2013-10-23 00:34:32 +0100

    Palindromic Detector

---
+++ src/puzzle_solutions/palindromic_detector.clj
@@ -0,0 +1,13 @@
+(ns puzzle-solutions.palindromic-detector
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 27 - Palindromic Detector
+;; http://www.4clojure.com/problem/27
+
+(def __ #(= (reverse (seq %)) (seq %)))
+
+(is (false? (__ '(1 2 3 4 5))))
+(is (true? (__ "racecar")))
+(is (true? (__ [:foo :bar :foo])))
+(is (true? (__ '(1 1 3 3 1 1))))
+(is (false? (__ '(:a :b :c))))
