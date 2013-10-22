21d4fa2ed68fad436b82eaa5c52a17da6dc181a1
Neil Laurance <neil.laurance@gmail.com>
2013-10-23 00:38:44 +0100

    Partially Flatten a Sequence

--- src/puzzle_solutions/partially_flatten_a_sequence.clj
+++ src/puzzle_solutions/partially_flatten_a_sequence.clj
@@ -1,11 +1,17 @@
-(ns puzzle-solutions.partially-flatten-a-sequence)
+(ns puzzle-solutions.partially-flatten-a-sequence
+  (:require [clojure.test :refer [is]]))
 
 ;; Problem 93 - Partially Flatten a Sequence
 ;; http://www.4clojure.com/problem/93
 
-(defn p-flatten [x]
-  (filter #(and (sequential? %)
-                (every? (complement sequential?) %))
-          (tree-seq sequential? seq x)))
+(def __ (fn [x]
+          (filter #(and (sequential? %)
+                        (every? (complement sequential?) %))
+                  (tree-seq sequential? seq x))))
 
-(p-flatten [["Do"] ["Nothing"]])
+(is (= (__ [["Do"] ["Nothing"]])
+       [["Do"] ["Nothing"]]))
+(is (= (__ [[[[:a :b]]] [[:c :d]] [:e :f]])
+       [[:a :b] [:c :d] [:e :f]]))
+(is (= (__ '((1 2)((3 4)((((5 6)))))))
+       '((1 2)(3 4)(5 6))))
