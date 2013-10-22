63efab55ce39aeab50aa8d112028279673929c8e
Neil Laurance <neil.laurance@gmail.com>
2013-10-22 23:12:49 +0100

    Lists Conj

---
+++ src/puzzle_solutions/lists_conj.clj
@@ -0,0 +1,10 @@
+(ns puzzle-solutions.lists-conj
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 5 - Lists Conj
+;; http://www.4clojure.com/problem/5
+
+(def __ '(1 2 3 4))
+
+(is (= __ (conj '(2 3 4) 1)))
+(is (= __ (conj '(3 4) 2 1)))
