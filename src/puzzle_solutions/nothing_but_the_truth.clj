af1bbcc170a8f4bc7e4e6b666de3c05a7f3ee62e
Neil Laurance <neil.laurance@gmail.com>
2013-10-22 23:49:53 +0100

    Nothing but the truth

---
+++ src/puzzle_solutions/nothing_but_the_truth.clj
@@ -0,0 +1,9 @@
+(ns puzzle-solutions.nothing-but-the-truth
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 1 - Nothing but the truth
+;; http://www.4clojure.com/problem/1
+
+(def __ true)
+
+(is (= __ true))
