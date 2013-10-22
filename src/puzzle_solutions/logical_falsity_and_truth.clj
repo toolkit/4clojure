75b8f895cae5b40cff18deabccb788ec0f18b4bf
Neil Laurance <neil.laurance@gmail.com>
2013-10-22 23:19:49 +0100

    Logical Falsity and Truth

---
+++ src/puzzle_solutions/logical_falsity_and_truth.clj
@@ -0,0 +1,15 @@
+(ns puzzle-solutions.logical-falsity-and-truth
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 162 - Logical Falsity and Truth
+;; http://www.4clojure.com/problem/162
+
+(def __ 1)
+
+(is (= __ (if-not false 1 0)))
+(is (= __ (if-not nil 1 0)))
+(is (= __ (if true 1 0)))
+(is (= __ (if [] 1 0)))
+(is (= __ (if [0] 1 0)))
+(is (= __ (if 0 1 0)))
+(is (= __ (if 1 1 0)))
