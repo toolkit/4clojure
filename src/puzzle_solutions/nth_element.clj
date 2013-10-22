79b7a96d0001318c89cfd7f4a6b11ba8a2fdc307
Neil Laurance <neil.laurance@gmail.com>
2013-10-23 00:17:02 +0100

    Nth Element

---
+++ src/puzzle_solutions/nth_element.clj
@@ -0,0 +1,12 @@
+(ns puzzle-solutions.nth-element
+   (:require [clojure.test :refer [is]]))
+
+;; Problem 21 - Nth Element
+;; http://www.4clojure.com/problem/21
+
+(def __ #(last (take (inc %2) %1)))
+
+(is (= (__ '(4 5 6 7) 2) 6))
+(is (= (__ [:a :b :c] 0) :a))
+(is (= (__ [1 2 3 4] 1) 2))
+(is (= (__ '([1 2] [3 4] [5 6]) 2) [5 6]))
