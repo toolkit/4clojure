d87bd49c420327ea9239a2e9194bd55ce5d0e22a
Neil Laurance <neil.laurance@gmail.com>
2013-10-24 20:26:11 +0100

    Read Roman Numerals

--- src/puzzle_solutions/read_roman_numerals.clj
+++ src/puzzle_solutions/read_roman_numerals.clj
@@ -1,23 +1,18 @@
-(ns puzzle-solutions.read-roman-numerals)
+(ns puzzle-solutions.read-roman-numerals
+  (:require [clojure.test :refer [is]]))
 
 ;; Problem 92 - Read Roman Numerals
 ;; http://www.4clojure.com/problem/92
 
-(fn [n]
+(def __
+  (fn [x]
   (let [ks ["M" "CM" "D" "CD" "C" "XC" "L" "XL" "X" "IX" "V" "IV" "I"]
         vs [1000 900 500 400 100 90 50 40 10 9 5 4 1]
         roman (zipmap ks vs)
         pattern (re-pattern (apply str (interpose "|" ks)))]
-    (apply + (map roman (re-seq pattern n)))))
+    (apply + (map roman (re-seq pattern x))))))
 
-(defn read-roman [s]
-  (let [ks ["M" "CM" "D" "CD" "C" "XC" "L" "XL" "X" "IX" "V" "IV" "I"]
-        vs [1000 900 500 400 100 90 50 40 10 9 5 4 1]
-        roman (zipmap ks vs)
-        pattern (re-pattern (apply str (interpose "|" ks)))]
-    (apply + (map roman (re-seq pattern s)))))
-
-(read-roman "MCMLXXXIV")
-(read-roman "MMXIII")
-(read-roman "MCMLXXI")
-(read-roman "MCMXLV")
+(is (= 14 (__ "XIV")))
+(is (= 827 (__ "DCCCXXVII")))
+(is (= 3999 (__ "MMMCMXCIX")))
+(is (= 48 (__ "XLVIII")))
