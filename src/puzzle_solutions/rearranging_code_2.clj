422dd38e7cd190230f9614333552ba2cb27196f9
Neil Laurance <neil.laurance@gmail.com>
2013-10-24 20:33:58 +0100

    Rearranging Code: ->>

---
+++ src/puzzle_solutions/rearranging_code_2.clj
@@ -0,0 +1,9 @@
+(ns puzzle-solutions.rearranging-code-2
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 72 - Rearranging Code: ->>
+;; http://www.4clojure.com/problem/72
+
+(is (= (reduce + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+       (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (reduce +))
+       11))
