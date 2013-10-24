8985c4123134e02e5e45cc323775cb29fd449778
Neil Laurance <neil.laurance@gmail.com>
2013-10-24 20:22:50 +0100

    Read a binary number

---
+++ src/puzzle_solutions/read_a_binary_number.clj
@@ -0,0 +1,15 @@
+(ns puzzle-solutions.read-a-binary-number
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 122 - Read a binary number
+;; http://www.4clojure.com/problem/122
+
+(def __ #(Integer/parseInt % 2))
+
+(is (= 0      (__ "0")))
+(is (= 7      (__ "111")))
+(is (= 8      (__ "1000")))
+(is (= 9      (__ "1001")))
+(is (= 255    (__ "11111111")))
+(is (= 1365   (__ "10101010101")))
+(is (= 65535  (__ "1111111111111111")))
