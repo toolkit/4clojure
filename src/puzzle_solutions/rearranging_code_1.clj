4f9a1ae1f4b002237585d3d10edc54035e6de4bb
Neil Laurance <neil.laurance@gmail.com>
2013-10-24 20:30:04 +0100

    Rearranging Code: ->

---
+++ src/puzzle_solutions/rearranging_code_1.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.rearranging-code-1
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 71 - Rearranging Code: ->
+;; http://www.4clojure.com/problem/71
+
+(def __ last)
+
+(is (= (__ (sort (rest (reverse [2 5 4 1 3 6]))))
+       (-> [2 5 4 1 3 6] reverse rest sort __)
+       5))
