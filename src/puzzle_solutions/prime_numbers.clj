edc09651703abda94dccd858f3209d2d0b14ec21
Neil Laurance <neil.laurance@gmail.com>
2013-10-24 18:58:21 +0100

    Prime Numbers

---
+++ src/puzzle_solutions/prime_numbers.clj
@@ -0,0 +1,16 @@
+(ns puzzle-solutions.prime-numbers
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 67 - Prime Numbers
+;; http://www.4clojure.com/problem/67
+
+(def __
+  (fn [y]
+    (take y
+      (filter
+        (fn [x] (not-any? zero? (map #(mod x %) (range 2 x))))
+        (iterate inc 2)))))
+
+(is (= (__ 2) [2 3]))
+(is (= (__ 5) [2 3 5 7 11]))
+(is (= (last (__ 100)) 541))
