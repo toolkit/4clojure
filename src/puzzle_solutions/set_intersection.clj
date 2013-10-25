1122ecc0333de0b4bd925edc45d5d12d38b8432c
Neil Laurance <neil.laurance@gmail.com>
2013-10-25 20:07:10 +0100

    Set Intersection

---
+++ src/puzzle_solutions/set_intersection.clj
@@ -0,0 +1,12 @@
+(ns puzzle-solutions.set-intersection
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 81 - Set Intersection
+;; http://www.4clojure.com/problem/81
+
+(def __ (fn [set1 set2]
+          (into #{} (filter (partial contains? set1) set2))))
+
+(is (= (__ #{0 1 2 3} #{2 3 4 5}) #{2 3}))
+(is (= (__ #{0 1 2} #{3 4 5}) #{}))
+(is (= (__ #{:a :b :c :d} #{:c :e :a :f :d}) #{:a :c :d}))
