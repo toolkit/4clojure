bf323f57bea482813ebf9e19302bc6863b2ae3d2
Neil Laurance <neil.laurance@gmail.com>
2013-10-25 19:21:06 +0100

    Reverse a Sequence

---
+++ src/puzzle_solutions/reverse_a_sequence.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.reverse-a-sequence
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 23 - Reverse a Sequence
+;; http://www.4clojure.com/problem/23
+
+(def __ #(into () %))
+
+(is (= (__ [1 2 3 4 5]) [5 4 3 2 1]))
+(is (= (__ (sorted-set 5 7 2 7)) '(7 5 2)))
+(is (= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]]))
