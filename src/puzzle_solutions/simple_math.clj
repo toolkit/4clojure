7ff411839a1ab31ec2193e11b7a5cf7090b4e60f
Neil Laurance <neil.laurance@gmail.com>
2013-10-25 20:14:10 +0100

    Simple Math

---
+++ src/puzzle_solutions/simple_math.clj
@@ -0,0 +1,9 @@
+(ns puzzle-solutions.simple-math
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 2 - Simple Math
+;; http://www.4clojure.com/problem/2
+
+(def __ 4)
+
+(is (= (- 10 (* 2 3)) __))
