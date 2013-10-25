a649944d3e5049c9eef6355ce0fd9077603f6ae0
Neil Laurance <neil.laurance@gmail.com>
2013-10-25 20:00:47 +0100

    Sequences: Filter

---
+++ src/puzzle_solutions/sequences_filter.clj
@@ -0,0 +1,9 @@
+(ns puzzle-solutions.sequences-filter
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 18 - Sequences: Filter
+;; http://www.4clojure.com/problem/18
+
+(def __ [6 7])
+
+(is (= __ (filter #(> % 5) '(3 4 5 6 7))))
