d931fa41001efff425825c2e8b4f565f419abf58
Neil Laurance <neil.laurance@gmail.com>
2013-10-25 20:04:50 +0100

    Sequences: Rest

---
+++ src/puzzle_solutions/sequences_rest.clj
@@ -0,0 +1,9 @@
+(ns puzzle-solutions.sequences-rest
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 13 - Sequences: Rest
+;; http://www.4clojure.com/problem/13
+
+(def __ '(20 30 40))
+
+(is (= __ (rest [10 20 30 40])))
