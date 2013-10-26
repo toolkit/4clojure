00bf6e75b31fdc89c12d3809a87a425fcf34f51a
Neil Laurance <neil.laurance@gmail.com>
2013-10-26 22:30:49 +0100

    Sum of square of digits

---
+++ src/puzzle_solutions/sum_of_square_of_digits.clj
@@ -0,0 +1,18 @@
+(ns puzzle-solutions.sum-of-square-of-digits
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 120 - Sum of square of digits
+;; http://www.4clojure.com/problem/24
+
+(def __ (fn [s]
+          (count
+           (filter
+            (fn [x]
+              (let [digits (map #(- (int %) 48) (str x))
+                    sum (apply + (map #(* % %) digits))]
+                (< x sum))) s))))
+
+(is (= 8 (__ (range 10))))
+(is (= 19 (__ (range 30))))
+(is (= 50 (__ (range 500))))
+(is (= 50 (__ (range 1000))))
