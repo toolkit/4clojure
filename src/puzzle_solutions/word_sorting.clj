a3cc04bfc3bba9722a83bf3518b3d9a91df3ea43
Neil Laurance <neil.laurance@gmail.com>
2013-10-26 23:30:39 +0100

    Word Sorting

---
+++ src/puzzle_solutions/word_sorting.clj
@@ -0,0 +1,14 @@
+(ns puzzle-solutions.word-sorting
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 70 - Word Sorting
+;; http://www.4clojure.com/problem/70
+
+(def __ (fn [s] (sort-by #(.toLowerCase %) (re-seq #"\w+" s))))
+
+(is (= (__  "Have a nice day.")
+       ["a" "day" "Have" "nice"]))
+(is (= (__  "Clojure is a fun language!")
+       ["a" "Clojure" "fun" "is" "language"]))
+(is (= (__  "Fools fall for foolish follies.")
+       ["fall" "follies" "foolish" "Fools" "for"]))
