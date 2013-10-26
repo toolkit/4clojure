a70558b5018cdcdcdbaaba3c6b8bee6d93696608
Neil Laurance <neil.laurance@gmail.com>
2013-10-26 22:19:22 +0100

    Split by Type

---
+++ src/puzzle_solutions/split_by_type.clj
@@ -0,0 +1,11 @@
+(ns puzzle-solutions.split-by-type
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 50 - Split by Type
+;; http://www.4clojure.com/problem/50
+
+(def __ #(vals (group-by type %)))
+
+(is (= (set (__ [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]}))
+(is (= (set (__ [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]}))
+(is (= (set (__ [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]}))
