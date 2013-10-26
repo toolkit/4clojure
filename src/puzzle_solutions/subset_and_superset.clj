2a925120264510ddce70592c229e81e2cfbe1116
Neil Laurance <neil.laurance@gmail.com>
2013-10-26 22:23:53 +0100

    Subset and Superset

---
+++ src/puzzle_solutions/subset_and_superset.clj
@@ -0,0 +1,12 @@
+(ns puzzle-solutions.subset-and-superset
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 161 - Subset and Superset
+;; http://www.4clojure.com/problem/161
+
+(def __ #{1 2})
+
+(is (clojure.set/superset? __ #{2}))
+(is (clojure.set/subset? #[1] __))
+(is (clojure.set/superset? __ #{1 2}))
+(is (clojure.set/subset? #{1 2} __))
