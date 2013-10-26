3f8e93feedcc7240f7d571b953b3c13757197e32
Neil Laurance <neil.laurance@gmail.com>
2013-10-26 23:18:56 +0100

    Tricky Card Games

---
+++ src/puzzle_solutions/tricky_card_games.clj
@@ -0,0 +1,21 @@
+(ns puzzle-solutions.tricky-card-games
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 141 - Tricky Card Games
+;; http://www.4clojure.com/problem/141
+
+(def __
+  (fn [trumps]
+    (fn [cards]
+      (last (sort-by :rank (filter #(= (or trumps (:suit (first cards))) (:suit %)) cards))))))
+
+(is (let [notrump (__ nil)]
+      (and (= {:suit :club :rank 9}  (notrump [{:suit :club :rank 4}
+                                               {:suit :club :rank 9}]))
+           (= {:suit :spade :rank 2} (notrump [{:suit :spade :rank 2}
+                                               {:suit :club :rank 10}])))))
+(is (= {:suit :club :rank 10} ((__ :club) [{:suit :spade :rank 2}
+                                           {:suit :club :rank 10}])))
+(is (= {:suit :heart :rank 8}
+       ((__ :heart) [{:suit :heart :rank 6} {:suit :heart :rank 8}
+                     {:suit :diamond :rank 10} {:suit :heart :rank 4}])))
