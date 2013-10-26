a1a45b3035f349c96eeaceef86a9fc216e07c6d9
Neil Laurance <neil.laurance@gmail.com>
2013-10-26 22:45:33 +0100

    The balance of N

---
+++ src/puzzle_solutions/the_balance_of_n.clj
@@ -0,0 +1,22 @@
+(ns puzzle-solutions.the-balance-of-n
+  (:require [clojure.test :refer [is]]))
+
+;; Problem 115 - The balance of N
+;; http://www.4clojure.com/problem/115
+
+(def __ (fn [x]
+          (let [m (zipmap (str "0123456789") (range))
+                d (map #(m %) (str x))
+                l (/ (count d) 2)]
+            (= (apply + (take l d))
+               (apply + (take-last l d))))))
+
+(is (= true (__ 11)))
+(is (= true (__ 121)))
+(is (= false (__ 123)))
+(is (= true (__ 0)))
+(is (= false (__ 88099)))
+(is (= true (__ 89098)))
+(is (= true (__ 89089)))
+(is (= (take 20 (filter __ (range)))
+       [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101]))
