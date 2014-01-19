de06a0b146ef63f02690079bf98182ee33ee62c4
Neil Laurance <neil.laurance@gmail.com>
2014-01-19 20:12:37 +0000

    formatting

--- src/puzzle_solutions/oscilrate.clj
+++ src/puzzle_solutions/oscilrate.clj
@@ -10,5 +10,4 @@
 
 (is (= (take 3 (__ 3.14 int double)) [3.14 3 3.0]))
 (is (= (take 5 (__ 3 #(- % 3) #(+ 5 %))) [3 0 5 2 7]))
-(is (= (take 12 (__ 0 inc dec inc dec inc)) [0 1 0 1 0 1 2 1 2 1 2 3])
-)
+(is (= (take 12 (__ 0 inc dec inc dec inc)) [0 1 0 1 0 1 2 1 2 1 2 3]))
