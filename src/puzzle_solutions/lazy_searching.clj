08e0c809310cf42b1f376fe51831067cb226db22
Neil Laurance <neil.laurance@gmail.com>
2014-01-19 20:03:10 +0000

    removed comments that were causing loading errors

--- src/puzzle_solutions/lazy_searching.clj
+++ src/puzzle_solutions/lazy_searching.clj
@@ -15,6 +15,6 @@
 (is (= 3 (__ [3 4 5])))
 (is (= 4 (__ [1 2 3 4 5 6 7] [0.5 3/2 4 19])))
 (is (= 7 (__ (range) (range 0 100 7/6) [2 3 5 7 11 13])))
-(is (= 64 (__ (map #(* % % %) (range)) ;; perfect cubes
-          (filter #(zero? (bit-and % (dec %))) (range)) ;; powers of 2
-          (iterate inc 20))) ;; at least as large as 20)
+(is (= 64 (__ (map #(* % % %) (range))
+          (filter #(zero? (bit-and % (dec %))) (range))
+          (iterate inc 20)))
