e7b5b63aa16e05d6295ab3556623c44579d69e8a
Neil Laurance <neil.laurance@gmail.com>
2014-01-19 20:26:37 +0000

    indentation

--- src/puzzle_solutions/read_roman_numerals.clj
+++ src/puzzle_solutions/read_roman_numerals.clj
@@ -6,11 +6,11 @@
 
 (def __
   (fn [x]
-  (let [ks ["M" "CM" "D" "CD" "C" "XC" "L" "XL" "X" "IX" "V" "IV" "I"]
-        vs [1000 900 500 400 100 90 50 40 10 9 5 4 1]
-        roman (zipmap ks vs)
-        pattern (re-pattern (apply str (interpose "|" ks)))]
-    (apply + (map roman (re-seq pattern x))))))
+    (let [ks ["M" "CM" "D" "CD" "C" "XC" "L" "XL" "X" "IX" "V" "IV" "I"]
+          vs [1000 900 500 400 100 90 50 40 10 9 5 4 1]
+          roman (zipmap ks vs)
+          pattern (re-pattern (apply str (interpose "|" ks)))]
+      (apply + (map roman (re-seq pattern x))))))
 
 (is (= 14 (__ "XIV")))
 (is (= 827 (__ "DCCCXXVII")))
