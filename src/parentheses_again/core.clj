6216e698bbc6442728ee26dfb5705355f7b2ab67
Neil Laurance <neil.laurance@gmail.com>
2015-06-14 13:08:43 +0100

    Parentheses again

--- src/parentheses_again/core.clj
+++ src/parentheses_again/core.clj
@@ -4,32 +4,22 @@
 ;; Problem 195 - Parentheses .. again
 ;; https://www.4clojure.com/problem/195
 
+(defn to-bits [x n]
+  (map #(bit-and 1 (bit-shift-right x %)) (range (- n 1) -1 -1)))
 
-(def __
-  (fn [n]
-    (let [to-bits (fn [x n]
-                    (map #(bit-shift-right (bit-and x (bit-shift-left 1 %)) %)
-                         (range (- n 1) -1 -1)))
-          balanced (fn [bits]
-                     (let [accum (take-while (complement neg?)
-                                             (reductions (fn [state bit] (+ state (case bit 1 1 0 -1))) bits))]
-                       (and (= (count bits) (count accum))
-                            (zero? (last accum)))))
-          to-parens (fn [bits] (apply str (map {1 \( 0 \)} bits)))
-          max (bit-shift-left 1 (* 2 n))]
-      (apply hash-set (for [x (range (/ max 2) max 2)
-                            :let [bits (to-bits x (* 2 n))]
-                            :when (balanced bits)]
-                        [(apply str (to-parens bits)) (apply str bits) x])))))
+(to-bits 2r11011011 8)
 
-
-(clojure.pprint/pprint (sort-by first (__ 4)))
-
-
-
+(def __ (fn [n]
+          (let [ins (fn [string pos]
+                      (apply str (subs string 0 pos) "()" (subs string pos)))
+                each-ins (fn [string]
+                           (map (partial ins string) (range 0 (inc (count string)))))
+                step (fn [string-set]
+                       (into #{} (flatten (map each-ins string-set))))]
+            (last (take (inc n) (iterate step #{""}))))))
 
 (deftest tests
-  ;(is (= [#{""} #{"()"} #{"()()" "(())"}] (map (fn [n] (__ n)) [0 1 2])))
+  (is (= [#{""} #{"()"} #{"()()" "(())"}] (map (fn [n] (__ n)) [0 1 2])))
   (is (= #{"((()))" "()()()" "()(())" "(())()" "(()())"} (__ 3)))
   (is (= 16796 (count (__ 10))))
   (is (= (nth (sort (filter #(.contains ^String % "(()()()())") (__ 9))) 6) "(((()()()())(())))"))
