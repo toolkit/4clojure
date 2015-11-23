a8d2d3f2fd2be03699a3e88188c60304e0884d3f
Neil Laurance <neil.laurance@gmail.com>
2015-11-23 00:48:38 +0000

    tidied up

--- src/word_chains/core.clj
+++ src/word_chains/core.clj
@@ -30,23 +30,23 @@
   (into #{} (filter (fn [[x y]] (= 1 (lev x y))) (pairs s))))
 
 (facts "about levenshtein distance"
-       (lev "abc" "ab") => 1
-       (lev "abb" "abc") => 1
+       (lev "abc" "ab")     => 1
+       (lev "abb" "abc")    => 1
        (lev "abc" "abcdef") => 3)
 
 (facts "about pairs"
-       (pairs #{1 2 3}) => #{[1 2] [1 3] [2 3]}
-       (pairs #{"abc" "def" "ghi" "jkl"})
-        => #{["abc" "def"] ["abc" "ghi"] ["abc" "jkl"] ["def" "ghi"] ["def" "jkl"] ["ghi" "jkl"]})
+       (pairs #{1 2 3})                   => #{[1 2] [1 3] [2 3]}
+       (pairs #{"abc" "def" "ghi" "jkl"}) => #{["abc" "def"] ["abc" "ghi"] ["abc" "jkl"]
+                                               ["def" "ghi"] ["def" "jkl"] ["ghi" "jkl"]})
 
 (facts "about just-ones"
-       (just-ones #{"abc" "ab" "abcd" "zzz" "kkk" "lll"})
-        => #{["ab" "abc"] ["abc" "abcd"]}
-       (just-ones #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})
-        => #{["cat" "hat"] ["hat" "oat"] ["hat" "hot"] ["coat" "oat"]
-             ["coat" "cot"] ["cat" "cot"] ["cat" "oat"] ["cat" "coat"]
-             ["dog" "hog"] ["cot" "hot"] ["hog" "hot"] })
-
+       (just-ones #{"abc" "ab" "abcd" "zzz" "kkk" "lll"})               => #{["ab" "abc"] ["abc" "abcd"]}
+       (just-ones #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})  => #{["cat" "hat"] ["hat" "oat"]
+                                                                             ["hat" "hot"] ["coat" "oat"]
+                                                                             ["coat" "cot"] ["cat" "cot"]
+                                                                             ["cat" "oat"] ["cat" "coat"]
+                                                                             ["dog" "hog"] ["cot" "hot"]
+                                                                             ["hog" "hot"] })
 (deftest tests
   (is (= true (__ #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
   (is (= false (__ #{"cot" "hot" "bat" "fat"})))
