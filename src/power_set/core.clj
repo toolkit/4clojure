2c142fb94dc3cb69e358e57277336d01b23e7923
laurann <neil.laurance@ig.com>
2016-05-04 10:18:48 +0100

    provided explanation of powerset solution

--- src/power_set/core.clj
+++ src/power_set/core.clj
@@ -10,6 +10,19 @@
               (reduce (fn [ps s]
                         (conj ps (conj s x))) ps ps)) #{#{}} s)))
 
+(comment "explanation of the solution above"
+         (defn powerset [initial-set]
+           (reduce (fn [the-current-powerset element]
+                     (println "current powerset:" the-current-powerset " element: " element)
+                     (reduce (fn [the-current-set the-set]
+                               (conj the-current-set (conj the-set element)))
+                             the-current-powerset
+                             the-current-powerset))
+                   #{#{}}
+                   initial-set))
+
+         (def __ powerset))
+
 (deftest tests
   (is (= (__ #{1 :a}) #{#{1 :a} #{:a} #{} #{1}}))
   (is (= (__ #{}) #{#{}}))
