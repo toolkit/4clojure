2808da91c50a991574b232b279df63aa76194114
Neil Laurance <neil.laurance@gmail.com>
2016-05-06 07:53:48 +0100

    squares squared resolved!

--- src/squares_squared/core.clj
+++ src/squares_squared/core.clj
@@ -15,75 +15,76 @@
 ;; of the shape and working outwards, with the initial direction being down and to the right.
 
 ;; Origin Offsets:
-;;                  0123    ----> x
+;;                  0123    ----> y
 ;;           -2 "   0   "
 ;;      |    -1 "  1 0  "
 ;;      |     0 " 0 1 0 "
 ;;      |     1 "* 0 0 0"
 ;;     \|/    2 " * 1 * "
-;;      y     3 "  * *  "
+;;      x     3 "  * *  "
 ;;            4 "   *   "
 
+;; Normalized:
+;;               0123456    ----> y
+;;            0 "   0   "
+;;      |     1 "  1 0  "
+;;      |     2 " 0 1 0 "
+;;      |     3 "* 0 0 0"
+;;     \|/    4 " * 1 * "
+;;      x     5 "  * *  "
+;;            6 "   *   "
+
+;;
+;; As one enourmous function
+;;
 (def __ (fn [start end]
-          (let [nums (take-while #(<= % end) (iterate #(* % %) start))
-                pad-length (first (filter #(< (count (apply str nums)) %) (map #(* % %) (range))))
-                padded (take pad-length (apply str (apply str nums) (repeat pad-length "*")))]
-            padded)))
-
-(__ 2 1024)
-
-
-;; explained in functions
-(defn squares [start end]
-  (take-while #(<= % end) (iterate #(* % %) start)))
-
-(defn pad-length [nums]
-  (first (filter #(< (count (apply str nums)) %) (map #(* % %) (range)))))
-
-(defn padded [nums length]
-  (take length (apply str (apply str nums) (repeat length "*"))))
-
-(defn add-points [[x1 y1] [x2 y2]] [(+ x1 x2) (+ y1 y2)])
-
-(def directions (cycle [[1 1] [-1 1] [-1 -1] [1 -1]]))
-
-(defn origin-offsets [n]
-  (loop [steps (dec n)
-         accum [[0 0]]
-         current-dir nil
-         next-dirs directions]
-    (if (zero? steps)
-      accum
-      (let [test-offset (add-points (last accum) (first next-dirs))]
-        (if (some #{test-offset} accum)
-          (recur (dec steps) (conj accum (add-points (last accum) current-dir)) current-dir next-dirs)
-          (recur (dec steps) (conj accum test-offset) (first next-dirs) (drop 1 next-dirs)))))))
-
-(deftest origin-offsets-test
-  (is (= [[0 0] [1 1] [0 2] [-1 1] [-2 0] [-1 -1] [0 -2] [1 -1] [2 0]]
-         (origin-offsets 9))))
-
-(defn grid-size [offsets]
-  (let [max-x (apply max (map first offsets))
-        min-x (apply min (map first offsets))
-        max-y (apply max (map second offsets))
-        min-y (apply min (map second offsets))]
-    (max (inc (- max-x min-x))
-         (inc (- max-y min-y)))))
-
-(defn empty-grid [size]
-  (vec (repeat size (repeat size " "))))
-
-(def g (empty-grid 7))
-
-(defn update-grid [grid offset val]
-  (assoc-in grid offset val))
-
-(update-grid g [1 1] "A")
+          (let [squares (fn [s e]
+                          (take-while #(<= % e) (iterate #(* % %) s)))
+                pad-length (fn [nums]
+                             (first (filter #(<= (count (apply str nums)) %) (map #(* % %) (range)))))
+                padded (fn [nums length]
+                         (take length (apply str (apply str nums) (repeat length "*"))))
+                add-points (fn [[x1 y1] [x2 y2]] [(+ x1 x2) (+ y1 y2)])
+                sub-points (fn [[x1 y1] [x2 y2]] [(- x1 x2) (- y1 y2)])
+                directions (cycle [[1 1] [1 -1] [-1 -1] [-1 1]])
+                origin-offsets (fn [n]
+                                 (loop [steps (dec n)
+                                        accum [[0 0]]
+                                        current-dir nil
+                                        next-dirs directions]
+                                   (if (zero? steps)
+                                     accum
+                                     (let [test-offset (add-points (last accum) (first next-dirs))]
+                                       (if (some #{test-offset} accum)
+                                         (recur (dec steps) (conj accum (add-points (last accum) current-dir)) current-dir next-dirs)
+                                         (recur (dec steps) (conj accum test-offset) (first next-dirs) (drop 1 next-dirs)))))))
+                normalize (fn [offsets]
+                            (let [min-x (apply min (map first offsets))
+                                  min-y (apply min (map second offsets))]
+                              (map #(sub-points % [min-x min-y]) offsets)))
+                grid-size (fn [normalized]
+                            (inc (apply max (map first normalized))))
+                empty-grid (fn [size]
+                             (vec (repeat size (vec (repeat size " ")))))
+                normalized-with-vals (fn [normalized vals]
+                                       (zipmap normalized vals))
+                update-grid (fn [grid nv]
+                              (reduce #(assoc-in % (first %2) (second %2)) grid nv))
+                str-grid (fn [grid]
+                           (map #(apply str %) grid))
 
 
-(origin-offsets 16)
-(origin-offsets 4)
+                sqs (squares start end)
+                len (pad-length sqs)
+                pad (padded sqs len)
+                off (origin-offsets len)
+                nrm (normalize off)
+                gsz (grid-size nrm)
+                grid (empty-grid gsz)
+                nv (normalized-with-vals nrm pad)
+                grid (update-grid grid nv)
+                grid (str-grid grid)]
+            grid)))
 
 (deftest tests
   (is (= (__ 2 2) ["2"]))
