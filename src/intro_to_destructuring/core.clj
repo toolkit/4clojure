c8c6953c184ac214d865b8b62082e5d0851f6dbc
Neil Laurance <neil.laurance@gmail.com>
2016-05-19 22:41:44 +0100

    fixed up intro to destructuring

--- src/intro_to_destructuring/core.clj
+++ src/intro_to_destructuring/core.clj
@@ -1,8 +1,8 @@
 (ns intro-to-destructuring.core
   (:require [clojure.test :refer :all]))
 
-;; Problem 53 - Intro to Destructuring
-;; http://www.4clojure.com/problem/53
+;; Problem 52 - Intro to Destructuring
+;; http://www.4clojure.com/problem/52
 
 (deftest tests
   (is (= [2 4] (let [[a b c d e f g] (range)] [c e]))))
