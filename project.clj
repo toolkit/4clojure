d194ceffaf2471f97155197abd5b69a33795bf91
Neil Laurance <neil.laurance@gmail.com>
2016-05-29 18:15:26 +0100

    Changed clojure version to 1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject puzzles "0.1.0-SNAPSHOT"
   :plugins [[lein-sub "0.3.0"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [midje "1.8.2"]])
