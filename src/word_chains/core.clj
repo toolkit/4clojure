0b2ff041a9aa0d67c2d68d4a2bafa6cee0075e7d
laurann <neil.laurance@ig.com>
2016-06-03 08:59:45 +0100

    word chains

--- src/word_chains/core.clj
+++ src/word_chains/core.clj
@@ -1,8 +1,18 @@
 (ns word-chains.core
-  (:use [midje.sweet]))
+  (:require [clojure.test :refer :all]))
 
 ;; Problem 82 - Word Chains
 ;; http://www.4clojure.com/problem/82
+;;
+;; Not a word chain:
+;;    to -- top -- stop
+;;           |
+;;          tops -- toss
+;;
+;; Is a word chain:
+;;    share -- shares -- hares -- hare -- are
+;;      |                           |
+;;       ----------------------------
 
 (def __ (fn [_] false))
 
@@ -22,36 +32,17 @@
                         (+ cost (mem mem (butlast s) (butlast t))))))))]
     (mlev mlev s t)))
 
-(defn pairs [s]
-  (into #{} (for [x s y s :when (neg? (compare x y))] [x y])))
+(defn edges [s]
+  (into #{} (for [x s y s :when (not= x y)] {:x x :y y :lev (lev x y)})))
 
-(defn just-ones [s]
-  (into #{} (map (partial apply hash-set)
-                 (filter (fn [[x y]] (= 1 (lev x y))) (pairs s)))))
+(edges #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})
 
-(defn nodes-possibly-connected [s]
-  (into #{} (mapcat identity (just-ones s))))
+(group-by :lev (edges #{"share" "hares" "shares" "hare" "are"}))
 
-(facts "about levenshtein distance"
-       (lev "abc" "ab")     => 1
-       (lev "abb" "abc")    => 1
-       (lev "abc" "abcdef") => 3)
-
-(facts "about pairs"
-       (pairs #{1 2 3})                   => #{[1 2] [1 3] [2 3]}
-       (pairs #{"abc" "def" "ghi" "jkl"}) => #{["abc" "def"] ["abc" "ghi"] ["abc" "jkl"]
-                                               ["def" "ghi"] ["def" "jkl"] ["ghi" "jkl"]})
-
-(facts "about just-ones"
-       (just-ones #{"abc" "ab" "abcd" "zzz" "kkk" "lll"}) => #{#{"ab" "abc"} #{"abc" "abcd"}})
-
-(facts "about nodes possibly connected"
-       (nodes-possibly-connected #{"aa" "ab" "ac" "zz"}) => #{"aa" "ab" "ac"})
-
-(facts "about word chains"
-       (__ #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"}) => true
-       (__ #{"cot" "hot" "bat" "fat"})                          => false
-       (__ #{"to" "top" "stop" "tops" "toss"})                  => false
-       (__ #{"spout" "do" "pot" "pout" "spot" "dot"})           => true
-       (__ #{"share" "hares" "shares" "hare" "are"})            => true
-       (__ #{"share" "hares" "hare" "are"})                     => false)
+(deftest tests
+  (is (true? (__ #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"})))
+  (is (false? (__ #{"cot" "hot" "bat" "fat"})))
+  (is (false? (__ #{"to" "top" "stop" "tops" "toss"})))
+  (is (true? (__ #{"spout" "do" "pot" "pout" "spot" "dot"})))
+  (is (true? (__ #{"share" "hares" "shares" "hare" "are"})))
+  (is (false? (__ #{"share" "hares" "hare" "are"}))))
