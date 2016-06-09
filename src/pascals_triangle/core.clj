9ee402d9dfb1e7794b9fb6bbb6dfcea112a087f9
laurann <neil.laurance@ig.com>
2016-06-09 13:33:56 +0100

    improved pascal's triangle solution

--- src/pascals_triangle/core.clj
+++ src/pascals_triangle/core.clj
@@ -5,18 +5,15 @@
 ;; http://www.4clojure.com/problem/97
 
 (def __ (fn [n]
-          (-> (iterate (fn [row]
-                         (map + `(0 ~@row) `(~@row 0)))
-                       [1])
-              (nth (dec n)))))
+          (nth (iterate #(map + `(0 ~@%) `(~@% 0)) [1]) (dec n))))
 
 (deftest tests
   (is (= (__ 1) [1]))
   (is (= (map __ (range 1 6))
-         [     [1]
-               [1 1]
-               [1 2 1]
-               [1 3 3 1]
-               [1 4 6 4 1]]))
+         [[1]
+          [1 1]
+          [1 2 1]
+          [1 3 3 1]
+          [1 4 6 4 1]]))
   (is (= (__ 11)
          [1 10 45 120 210 252 210 120 45 10 1])))
