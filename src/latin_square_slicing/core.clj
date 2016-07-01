9fd1eab287ceba1f4371c8a753d2ced7b04370a6
Neil Laurance <neil.laurance@gmail.com>
2016-07-01 23:20:06 +0100

    latin square slicing tests

--- src/latin_square_slicing/core.clj
+++ src/latin_square_slicing/core.clj
@@ -15,11 +15,9 @@
 
 (is-latin [[1 2] [2 1]])
 
-
 (distinct [1 2 2])
 
-
-(def mat [ [1 2 3] [2 3 1] [3 1 2]])
+(def mat [[1 2 3] [2 3 1] [3 1 2]])
 
 (def a (partition 2 1 mat))
 
@@ -41,10 +39,56 @@
         parts (partial partition size 1)
         small-squares (fn [part]
                         (transpose (map #(parts %) part)))]
-      (mapcat small-squares (parts matrix))))
+    (mapcat small-squares (parts matrix))))
 
 (squares-for mat 2)
 
 (keep is-latin (squares-for mat 2))
 
 (map #(keep is-latin (squares-for mat %)) (range 2 (inc (count mat))))
+
+(def __ (fn []))
+
+(deftest tests
+  (is (= (__ '[[A B C D]
+               [A C D B]
+               [B A D C]
+               [D C A B]])
+         {}))
+  (is (= (__ '[[A B C D]
+               [A C D B]
+               [B A D C]
+               [D C A B]])
+         {}))
+  (is (= (__ '[[A B C D]
+               [B A D C]
+               [D C B A]
+               [C D A B]])
+         {4 1, 2 4}))
+  (is (= (__ '[[B D A C B]
+               [D A B C A]
+               [A B C A B]
+               [B C A B C]
+               [A D B C A]])
+         {3 3}))
+  (is (= (__ [[2 4 6 3]
+              [3 4 6 2]
+              [6 2 4]])
+         {}))
+  (is (= (__ [[1]
+              [1 2 1 2]
+              [2 1 2 1]
+              [1 2 1 2]
+              []])
+         {2 2}))
+  (is (= (__ [[3 1 2]
+              [1 2 3 1 3 4]
+              [2 3 1 3]])
+         {3 1, 2 2}))
+  (is (= (__ [[8 6 7 3 2 5 1 4]
+              [6 8 3 7]
+              [7 3 8 6]
+              [3 7 6 8 1 4 5 2]
+              [1 8 5 2 4]
+              [8 1 2 4 5]])
+         {4 1, 3 1, 2 7})))
