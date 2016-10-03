4c037dfaedd8d57eaf0c8845cc40eacd92db17d5
Neil Laurance <neil.laurance@ig.com>
2016-10-03 14:04:02 +0100

    Better solution for global take while supports infinite sequences

--- src/global_take_while/core.clj
+++ src/global_take_while/core.clj
@@ -4,15 +4,22 @@
 ;; Problem 114 - Global take while
 ;; http://www.4clojure.com/problem/114
 
-(def __ (fn [n p s]
-          (take
-           (last
-            (take n (keep-indexed #(if (p %2) %) s)))
-           s)))
+(def __
+  (fn gtw
+    ([n pred coll] (gtw n pred coll 0))
+    ([n pred coll matched]
+     (let [item (first coll)]
+       (when item
+         (let [matched (if (pred item) (inc matched) matched)]
+           (when (< matched n)
+             (lazy-seq (cons item (gtw n pred (rest coll) matched))))))))))
 
 (deftest tests
   (is (= [2 3 5 7 11 13]
          (__ 4 #(= 2 (mod % 3))
+             [2 3 5 7 11 13 17 19 23])))
+  (is (= [2 3 5 7 11 13 17 19 23]
+         (__ 4 #(= 0 (mod % 71))
              [2 3 5 7 11 13 17 19 23])))
   (is (= ["this" "is" "a" "sentence"]
          (__ 3 #(some #{\i} %)
